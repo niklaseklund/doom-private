42fab09a25cc19146221adfd7685fd0b62a1995e
Niklas Carlsson <carlsson.niklas@gmail.com>
2018-03-23 07:34:53 +0100

    Extend with unimpaired mappings
    I am adding an unimpaired mapping that I have been missing which is to open up a
    line above or below my current point.

--- config.el
+++ config.el
@@ -1,29 +1,27 @@
+;;; config.el -*- lexical-binding: t; -*-
+
+;; This file keeps my custom settings
+
 ;; Zen experienced writing
 (def-package! writeroom-mode)
 
+;; Custom functions
+(defun evil-unimpaired/insert-space-above (count)
+  (interactive "p")
+  (dotimes (_ count) (save-excursion (evil-insert-newline-above))))
+
+(defun evil-unimpaired/insert-space-below (count)
+  (interactive "p")
+  (dotimes (_ count) (save-excursion (evil-insert-newline-below))))
+
 ;; Override other keys
-(map! :leader
-      :prefix "g"
+(map! (:leader
+      ;; Git
+      (:prefix "g"
       :n "s" #'magit-status
-      :n "S" #'git-gutter:stage-hunk)
-
-;; Different languages in insert and normal state
-;; (setq lang_source "com.apple.keylayout.US")                     ;set default var lang_source for issw arg
-;; (add-hook 'evil-insert-state-entry-hook                         ;what we do when enter insert mode
-;;           (lambda ()
-;;             (shell-command (concat "issw " lang_source))))      ;
-;; ;
-;; (add-hook 'evil-insert-state-exit-hook                          ;what we do when enter normal mode
-;;           (lambda ()
-;;             (setq lang_source (shell-command-to-string "issw"))
-;;             (shell-command "issw com.apple.keylayout.US")))
-
-;; (setq lang_source "com.apple.keylayout.US")                     ;set default var lang_source for issw arg
-;; (add-hook 'evil-replace-state-entry-hook                         ;what we do when enter insert mode
-;;           (lambda ()
-;;             (shell-command (concat "issw " lang_source))))      ;
-;; ;
-;; (add-hook 'evil-replace-state-exit-hook                          ;what we do when enter normal mode
-;;           (lambda ()
-;;             (setq lang_source (shell-command-to-string "issw"))
-;;             (shell-command "issw com.apple.keylayout.US")))
+      :n "S" #'git-gutter:stage-hunk))
+      ;; Vimesque keys
+      (:prefix "["
+      :n "SPC" #'evil-unimpaired/insert-space-above)
+      (:prefix "]"
+      :n "SPC" #'evil-unimpaired/insert-space-below))
