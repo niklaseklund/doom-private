99b85848dcaf2f2a5fdd6ea557c30b76db10f357
Niklas Carlsson <carlsson.niklas@gmail.com>
2018-04-05 00:04:18 +0200

    Make doom-private fully functional

---
+++ modules/private/niklascarlsson/init.el
@@ -0,0 +1,8 @@
+;;; config/niklascarlsson/init.el -*- lexical-binding: t; -*-
+
+;; Override the doom leader keys
+(setq doom-leader-key ","
+      doom-localleader-key ", m")
+
+;; Change the font
+(setq doom-font (font-spec :family "Roboto Mono" :size 15))
