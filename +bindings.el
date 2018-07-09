989a24464843da7e6dc0787c15cc156b3575efa0
Niklas Carlsson <carlsson.niklas@gmail.com>
2018-07-09 19:58:28 +0200

    Add bindings for org-noter

--- +bindings.el
+++ +bindings.el
@@ -17,6 +17,16 @@
         :desc "Zen writing" :n "z" #'writeroom-mode
         :desc "Wrap lines to fit screen" :n "v" #'visual-line-mode
         :desc "Column-indicator" :n "c" #'fci-mode)
+      ;; Noter
+      (:prefix "n"
+        :desc "Open org-noter" :n "o" #'org-noter
+        :desc "Kill org-noter session" :n "k" #'org-noter-kill-session
+        :desc "Insert org-note" :n "i" #'org-noter-insert-note
+        :desc "Insert precise org-note" :n "p" #'org-noter-insert-precise-note
+        :desc "Sync current note" :n "." #'org-noter-sync-current-note
+        :desc "Sync next note" :n "]" #'org-noter-sync-next-note
+        :desc "Sync previous note" :n "[" #'org-noter-sync-prev-note
+        )
       ;; Window
       (:prefix "w"
         :desc "Close all other windows" :n "O" #'delete-other-windows
