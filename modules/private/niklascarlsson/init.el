2ebf91c340dbdf7f7e9dd26af6e48611875e8c2c
Niklas Carlsson <carlsson.niklas@gmail.com>
2018-07-16 19:52:19 +0200

    Have different font sizes for work / home

--- modules/private/niklascarlsson/init.el
+++ modules/private/niklascarlsson/init.el
@@ -4,8 +4,14 @@
 (setq doom-leader-key ","
       doom-localleader-key ", m")
 
-;; Change the font
-(setq doom-font (font-spec :family "Roboto Mono" :size 15))
-;; Set theme
+;; Set the font
+(when IS-MAC
+  (setq doom-font (font-spec :family "Roboto Mono" :size 13))
+  )
+(when IS-LINUX
+  (setq doom-font (font-spec :family "Roboto Mono" :size 15))
+  )
+
+;; Set the theme
 (setq doom-theme 'doom-one)
 ;; (setq doom-theme 'doom-nord-light)
