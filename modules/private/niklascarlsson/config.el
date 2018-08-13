c930feb57d8108fca48f77420817d7148c4a3ded
Niklas Carlsson <niklas.carlsson@zenuity.com>
2018-08-13 16:10:02 +0200

    Add org-babel functions
    Nice to have functions which makes one rerun blocks up to point and also
    kill currently running sessions. Missing out on the kill and rerun
    command but that should be easy to build myself with the help of those
    other functions.

--- modules/private/niklascarlsson/config.el
+++ modules/private/niklascarlsson/config.el
@@ -521,4 +521,79 @@ and a backlink to the function and the file."
 (add-to-list 'org-capture-templates
              '("J" "Work journal" entry (file+olp+datetree "~/org/work/journal.org")
                "* %?\nEntered on %U\n %i\n %a"))
+
+;; Org-babel
+(defun src-block-in-session-p (&optional name)
+  "Return if src-block is in a session of NAME.
+NAME may be nil for unnamed sessions."
+  (let* ((info (org-babel-get-src-block-info))
+         (lang (nth 0 info))
+         (body (nth 1 info))
+         (params (nth 2 info))
+         (session (cdr (assoc :session params))))
+
+    (cond
+     ;; unnamed session, both name and session are nil
+     ((and (null session)
+           (null name))
+      t)
+     ;; Matching name and session
+     ((and
+       (stringp name)
+       (stringp session)
+       (string= name session))
+      t)
+     ;; no match
+     (t nil))))
+
+(defun org-babel-restart-session-to-point (&optional arg)
+  "Restart session up to the src-block in the current point.
+Goes to beginning of buffer and executes each code block with
+`org-babel-execute-src-block' that has the same language and
+session as the current block. ARG has same meaning as in
+`org-babel-execute-src-block'."
+  (interactive "P")
+  (unless (org-in-src-block-p)
+    (error "You must be in a src-block to run this command"))
+  (let* ((current-point (point-marker))
+         (info (org-babel-get-src-block-info))
+         (lang (nth 0 info))
+         (params (nth 2 info))
+         (session (cdr (assoc :session params))))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward org-babel-src-block-regexp nil t)
+        ;; goto start of block
+        (goto-char (match-beginning 0))
+        (let* ((this-info (org-babel-get-src-block-info))
+               (this-lang (nth 0 this-info))
+               (this-params (nth 2 this-info))
+               (this-session (cdr (assoc :session this-params))))
+            (when
+                (and
+                 (< (point) (marker-position current-point))
+                 (string= lang this-lang)
+                 (src-block-in-session-p session))
+              (org-babel-execute-src-block arg)))
+        ;; move forward so we can find the next block
+        (forward-line)))))
+
+(defun org-babel-kill-session ()
+  "Kill session for current code block."
+  (interactive)
+  (unless (org-in-src-block-p)
+    (error "You must be in a src-block to run this command"))
+  (save-window-excursion
+    (org-babel-switch-to-session)
+    (kill-buffer)))
+
+(defun org-babel-remove-result-buffer ()
+  "Remove results from every code block in buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward org-babel-src-block-regexp nil t)
+      (org-babel-remove-result))))
+
+
 ;;   :config
