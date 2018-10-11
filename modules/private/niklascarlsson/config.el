1cb30f289345eba13c500c8f02b351be124c77e3
Niklas Carlsson <niklas.carlsson@zenuity.com>
2018-10-11 17:49:08 +0200

    Add Hugo capture to store blog ideas
    Quick way of adding new blog post ideas which can later be refiled in org-mode.

--- modules/private/niklascarlsson/config.el
+++ modules/private/niklascarlsson/config.el
@@ -598,6 +598,32 @@ and a backlink to the function and the file."
 (add-to-list 'org-capture-templates
              '("J" "Work journal" entry (file+olp+datetree "~/org/work/journal.org")
                "* %?\nEntered on %U\n %i\n %a"))
+;; Hugo
+;; Populates only the EXPORT_FILE_NAME property in the inserted headline.
+(with-eval-after-load 'org-capture
+  (defun org-hugo-new-subtree-post-capture-template ()
+    "Returns `org-capture' template string for new Hugo post.
+See `org-capture-templates' for more information."
+    (let* ((title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
+           (fname (org-hugo-slug title)))
+      (mapconcat #'identity
+                 `(
+                   ,(concat "* TODO " title)
+                   ":PROPERTIES:"
+                   ,(concat ":EXPORT_FILE_NAME: " fname)
+                   ":END:"
+                   "%?\n")          ;Place the cursor here finally
+                 "\n")))
+
+  (add-to-list 'org-capture-templates
+               '("h"                ;`org-capture' binding + h
+                 "Hugo post"
+                 entry
+                 ;; It is assumed that below file is present in `org-directory'
+                 ;; and that it has a "Blog Ideas" heading. It can even be a
+                 ;; symlink pointing to the actual location of all-posts.org!
+                 (file+olp "todo.org" "Blog Ideas")
+                 (function org-hugo-new-subtree-post-capture-template))))
 
 ;; Org-babel
 (defun src-block-in-session-p (&optional name)
