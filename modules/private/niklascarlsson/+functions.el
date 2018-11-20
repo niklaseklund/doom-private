103437355024aee6c48e31341429491f0b0699a5
Niklas Carlsson <carlsson.niklas@gmail.com>
2018-11-20 20:40:16 +0100

    Add functions for docker integration in org-mode

--- modules/private/niklascarlsson/+functions.el
+++ modules/private/niklascarlsson/+functions.el
@@ -33,3 +33,24 @@
   (interactive "sEnter git branch pattern: ")
   (let ((branches (my/git-branch-match name-regexp)))
     (mapcar 'my/git-delete-branch branches)))
+
+
+(defun my/docker-match (name-regexp)
+  ;; return the name of the last docker image which matches the input
+  ;; NAME-REGEXP
+  (with-temp-buffer (shell-command "docker ps" t)
+                    (goto-char (point-min))
+                    (let ((name-match '()))
+                      (while (not (eobp))
+                        (let ((current-name (string-trim (thing-at-point 'line))))
+                          (if (string-match name-regexp current-name)
+                              (progn
+                                (end-of-line)
+                                (setq name-match (format "%s" (thing-at-point 'symbol))))))
+                        (forward-line 1))
+                      name-match)))
+
+(defun my/docker-path (name-regexp  &optional extended-path)
+  (if extended-path
+      (format "/docker:%s:/%s" (my/docker-match name-regexp) extended-path)
+    (format "/docker:%s:/" (my/docker-match name-regexp))))
