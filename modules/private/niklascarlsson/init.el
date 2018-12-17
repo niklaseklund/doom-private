6b0638003b420d77ce1c1d71f7dac27dacc1397b
Niklas Carlsson <niklas.carlsson@zenuity.com>
2018-12-17 09:50:42 +0100

    Make Dracula my default theme

--- modules/private/niklascarlsson/init.el
+++ modules/private/niklascarlsson/init.el
@@ -13,6 +13,6 @@
   )
 
 ;; Set the theme
-(setq doom-theme 'doom-one)
-;; (setq doom-theme 'doom-dracula)
+;; (setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
 ;; (setq doom-theme 'doom-nord-light)
