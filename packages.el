168442cc142974e23c9819c3797d07f834db3bc5
Niklas Carlsson <niklas.carlsson@zenuity.com>
2019-01-17 11:09:40 +0100

    The evil-matchit has been fixed upstream

--- packages.el
+++ packages.el
@@ -1,5 +1,4 @@
 ;;; packages.el -*- lexical-binding: t; -*-
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
 (package! cmake-mode :disable t)
 (package! company-irony :disable t)
 (package! company-irony-c-headers :disable t)
