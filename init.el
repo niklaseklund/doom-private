dc72db5eaf92386a087fdc1c8d018c9ec58c5dff
Niklas Carlsson <niklas.carlsson@zenuity.com>
2019-02-18 15:18:09 +0100

    Enable new fold module

--- init.el
+++ init.el
@@ -46,6 +46,7 @@
        window-select     ; visually switch windows
 
        :editor
+       fold              ; (nigh) universal code folding
        (format +onsave)  ; automated prettiness
        lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
@@ -60,7 +61,6 @@
        ediff             ; comparing files in Emacs
        electric          ; smarter, keyword-based electric-indent
        eshell            ; a consistent, cross-platform shell (WIP)
-       hideshow          ; basic code-folding support
        imenu             ; an imenu sidebar and searchable code index
        term              ; terminals in Emacs
        vc                ; version-control and Emacs, sitting in a tree
