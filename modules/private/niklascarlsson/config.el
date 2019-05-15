86f6c91a1b3dc60a77068558c2ad62877d8c97f8
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-05-15 13:41:24 +0200

    Make the calendar display weeks

--- modules/private/niklascarlsson/config.el
+++ modules/private/niklascarlsson/config.el
@@ -37,6 +37,17 @@
 (set-frame-parameter nil 'fullscreen 'maximized)
 
 
+;; Show week numbers in calendar
+  (setq calendar-week-start-day 1
+        calendar-intermonth-text
+        '(propertize
+          (format "%2d"
+                  (car
+                   (calendar-iso-from-absolute
+                    (calendar-absolute-from-gregorian (list month day year)))))
+          'font-lock-face 'font-lock-function-name-face))
+
+
 ;; Load custom functions
 (load! "+functions")
 
