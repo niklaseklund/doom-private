29dca55772a120db4b06c53721a18fb4f972d77a
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-05-24 10:54:05 +0200

    Update DAP configuration

--- modules/private/niklascarlsson/config.el
+++ modules/private/niklascarlsson/config.el
@@ -267,15 +267,15 @@
   ;; Debugger Cheatsheet
   (defhydra hydra-debugger-control (:color purple :hint nil :foreign-keys run)
     "
-^Stepping^             ^Switch^           ^Breakpoints^          ^Eval                         ^Debug
+^Stepping^             ^Switch^           ^Breakpoints^          ^Eval^                        ^Debug
 ^^^^^^^^------------------------------------------------------------------------------------------------------------
 _n_: Next          _ss_: Session          _bt_: Toggle          _ee_: Eval                     _dd_: Debug
 _i_: Step in       _st_: Thread           _bd_: Delete          _er_: Eval region              _de_: Debug edit
-_o_: Step out      _sf_: Stack frame      _ba_: Add             _es_: Eval thing at point      ^^
-_c_: Continue      _sl_: List locals      _bc_: Set condition   _eii_: Inspect                 ^^
-_r_: Restart frame _sb_: List breakpoints _bh_: Set hit count   _eir_: Inspect region          ^^
-_Q_: Disconnect    _sS_: List sessions    _bl_: Set log message _eis_: Inspect thing at point  ^^
-^^                 ^^                     _bD_: Delete all      ^^                             ^^
+_o_: Step out      _sf_: Stack frame      _ba_: Add             _es_: Eval thing at point      ^ ^
+_c_: Continue      _sl_: List locals      _bc_: Set condition   _eii_: Inspect                 ^ ^
+_r_: Restart frame _sb_: List breakpoints _bh_: Set hit count   _eir_: Inspect region          ^ ^
+_Q_: Disconnect    _sS_: List sessions    _bl_: Set log message _eis_: Inspect thing at point  ^ ^
+^ ^                 ^ ^                   _bD_: Delete all      ^ ^                            ^ ^
 "
     ("n" dap-next)
     ("i" dap-step-in)
@@ -342,6 +342,10 @@ _Q_: Disconnect    _sS_: List sessions    _bl_: Set log message _eis_: Inspect t
     (unless (-filter 'dap--session-running (dap--get-sessions))
       (and (get-buffer dap-ui--sessions-buffer)
            (kill-buffer dap-ui--sessions-buffer))
+      (and (get-buffer "*dap-ui-repl*")
+           (kill-buffer "*dap-ui-repl*"))
+      ;; (and (get-buffer "*compilation*")
+      ;;      (kill-buffer "*compilation*"))
       (and (get-buffer dap-ui--locals-buffer)
            (kill-buffer dap-ui--locals-buffer))))
 
@@ -349,48 +353,49 @@ _Q_: Disconnect    _sS_: List sessions    _bl_: Set log message _eis_: Inspect t
   (add-hook 'dap-stopped-hook '+dap/show-debug-windows)
   (add-hook 'dap-terminated-hook '+dap/hide-debug-windows)
 
-  (defvar +dap-running-session-mode-map
-    (let ((map (make-sparse-keymap)))
-      (define-key map (kbd "n") 'dap-next)
-      (define-key map (kbd "i") 'dap-step-in)
-      (define-key map (kbd "o") 'dap-step-out)
-      (define-key map (kbd "c") 'dap-continue)
-      (define-key map (kbd "r") 'dap-restart-frame)
-      (define-key map (kbd "Q") 'dap-disconnect)
-      (define-key map (kbd "b") 'dap-breakpoint-toggle)
-      (define-key map (kbd "B") 'dap-breakpoint-condition)
-      map)
-    "Keybindings for +dap-runnong-session-mode")
+  ;; (defvar +dap-running-session-mode-map
+  ;;   (let ((map (make-sparse-keymap)))
+  ;;     (define-key map (kbd "n") 'dap-next)
+  ;;     (define-key map (kbd "i") 'dap-step-in)
+  ;;     (define-key map (kbd "o") 'dap-step-out)
+  ;;     (define-key map (kbd "c") 'dap-continue)
+  ;;     (define-key map (kbd "r") 'dap-restart-frame)
+  ;;     (define-key map (kbd "Q") 'dap-disconnect)
+  ;;     (define-key map (kbd "b") 'dap-breakpoint-toggle)
+  ;;     (define-key map (kbd "B") 'dap-breakpoint-condition)
+  ;;     map)
+  ;;   "Keybindings for +dap-runnong-session-mode")
 
-  ;; activate minor modes when stepping through code
-  ;; https://github.com/emacs-lsp/dap-mode/wiki/How-to-activate-minor-modes-when-stepping-through-code
-  (define-minor-mode +dap-running-session-mode
-    "A mode for adding keybindings to running sessions"
-    nil
-    nil
-    +dap-running-session-mode-map
-    (with-eval-after-load 'evil
-      (evil-normalize-keymaps) ;; if you use evil, this is necessary to update the keymaps
-      (evil-make-overriding-map +dap-running-session-mode-map))
-    ;; The following code adds to the dap-terminated-hook
-    ;; so that this minor mode will be deactivated when the debugger finishes
-    (when +dap-running-session-mode
-      (let ((session-at-creation (dap--cur-active-session-or-die)))
-        (add-hook 'dap-terminated-hook
-                  (lambda (session)
-                    (when (eq session session-at-creation)
-                      (+dap-running-session-mode -1)))))))
+  ;; ;; activate minor modes when stepping through code
+  ;; ;; https://github.com/emacs-lsp/dap-mode/wiki/How-to-activate-minor-modes-when-stepping-through-code
+  ;; (define-minor-mode +dap-running-session-mode
+  ;;   "A mode for adding keybindings to running sessions"
+  ;;   nil
+  ;;   nil
+  ;;   +dap-running-session-mode-map
+  ;;   (with-eval-after-load 'evil
+  ;;     (evil-normalize-keymaps) ;; if you use evil, this is necessary to update the keymaps
+  ;;     (evil-make-overriding-map +dap-running-session-mode-map))
+  ;;   ;; The following code adds to the dap-terminated-hook
+  ;;   ;; so that this minor mode will be deactivated when the debugger finishes
+  ;;   (when +dap-running-session-mode
+  ;;     (let ((session-at-creation (dap--cur-active-session-or-die)))
+  ;;       (add-hook 'dap-terminated-hook
+  ;;                 (lambda (session)
+  ;;                   (when (eq session session-at-creation)
+  ;;                     (+dap-running-session-mode -1)))))))
 
-  ;; Activate this minor mode when dap is initialized
-  (add-hook 'dap-session-created-hook '+dap-running-session-mode)
+  ;; ;; Activate this minor mode when dap is initialized
+  ;; (add-hook 'dap-session-created-hook '+dap-running-session-mode)
 
-  ;; Activate this minor mode when hitting a breakpoint in another file
-  (add-hook 'dap-stopped-hook '+dap-running-session-mode)
+  ;; ;; Activate this minor mode when hitting a breakpoint in another file
+  ;; (add-hook 'dap-stopped-hook '+dap-running-session-mode)
 
-  ;; Activate this minor mode when stepping into code in another file
-  (add-hook 'dap-stack-frame-changed-hook (lambda (session)
-                                            (when (dap--session-running session)
-                                              (+dap-running-session-mode 1)))))
+  ;; ;; Activate this minor mode when stepping into code in another file
+  ;; (add-hook 'dap-stack-frame-changed-hook (lambda (session)
+  ;;                                           (when (dap--session-running session)
+  ;;                                             (+dap-running-session-mode 1))))
+)
 
 
 ;; eshell
