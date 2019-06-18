f7834ff4bc9f4f33b67c4ba2d8f805f0ab579765
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-06-18 15:44:48 +0200

    Try removing flycheck +childframe
    I have had some issues with the mouse cursor being misplaced when an
    error frame pops up. I will see how the behavior is when I don't use
    childframes.

--- init.el
+++ init.el
@@ -79,8 +79,7 @@
        ;; editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        eval              ; run code, run (also, repls)
-       (flycheck             ; tasing you for every semicolon you forget
-        +childframe)     ; use childframes for error popups (Emacs 26+ only)
+       flycheck             ; tasing you for every semicolon you forget
        flyspell          ; tasing you for misspelling mispelling
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
