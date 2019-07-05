0ef48fe813d544de16fc150cc98b44c893b74c73
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-07-05 14:04:15 +0200

    Make lsp ignore build folder
    Emacs has been unresponsive sometimes during compilation process. This
    seems to be because of all the file changes that occurs in the build
    folder. The solution is to add the build folder to an ignore list.
    
    See this issue for more information:
    https://github.com/emacs-lsp/lsp-mode/issues/748

--- +lsp.el
+++ +lsp.el
@@ -7,13 +7,7 @@
         lsp-ui-sideline-ignore-duplicate t
         lsp-ui-doc-enable nil
         lsp-ui-doc-include-signature nil
-        lsp-ui-doc-header nil))
-;; company
-;; (with-eval-after-load 'company-lsp
-  ;; (setq company-lsp-enable-snippet t))
-;; lsp-flycheck
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-after-open-hook (lambda () (lsp-ui-flycheck-enable t)))
-  (add-hook 'c-mode-common-hook 'flycheck-mode)) ;; Turn on flycheck for C++ buffers
+        lsp-ui-doc-header nil)
+  (add-to-list 'lsp-file-watch-ignored "build"))
 ;; lsp-formatting
 (add-hook 'before-save-hook (lambda ()  (when (eq major-mode 'python-mode) (lsp-format-buffer))))
