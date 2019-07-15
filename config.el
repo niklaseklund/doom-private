73ff2244fb5574af06636224a46ca658dd546b34
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-07-15 16:41:35 +0200

    Tidy up the config

--- config.el
+++ config.el
@@ -1,39 +1,23 @@
 ;;;  -*- lexical-binding: t; -*-
 
-;; TODO divide into different sections
-;; 1) user settings
-;; 2) emacs core settings
-;; 3) doom looks settings
-;; 4) doom core settings
-;; 5) doom package settings
-;; 6) custom package settings
-;; 7) load my own "module" files
-
-;; Load functions
-(load! "+functions")
 
 ;;
-;; User
+;; UI
 (setq user-full-name    "Niklas Carlsson"
       user-mail-address "carlsson.niklas@gmail.com")
 
-;;
-;; Looks
+;; looks
 (setq doom-modeline-height 40
       +doom-modeline-buffer-file-name-style 'relative-from-project
       doom-theme 'doom-dracula
-      show-trailing-whitespace nil)
+      +workspaces-switch-project-function #'ignore)
 
-;;
-;; General
-(setq confirm-kill-emacs nil
-      projectile-project-search-path '("~/src/" "~/opensource")
-      +workspaces-switch-project-function #'ignore
-      python-indent-guess-indent-offset nil)
+;; prettify modes
+(setq +pretty-code-enabled-modes '(emacs-lisp-mode org-mode))
 (set-frame-parameter nil 'fullscreen 'maximized)
 
-;;
-;; Host configuration
+;; host os configuration
+(load! "+functions")
 (when (my/os-match "ARCH")
   (if (my/multi-screen-setup-p)
       (setq doom-font (font-spec :family "Roboto Mono" :size 16)
@@ -54,28 +38,79 @@
                mac-option-modifier 'alt
                mac-command-modifier 'meta)
 
+
 ;;
-;; Calendar
+;; Core Emacs
+
 ;; Show week numbers in calendar
-  (setq calendar-week-start-day 1
-        calendar-intermonth-text
-        '(propertize
-          (format "%2d"
-                  (car
-                   (calendar-iso-from-absolute
-                    (calendar-absolute-from-gregorian (list month day year)))))
-          'font-lock-face 'font-lock-function-name-face))
+(setq calendar-week-start-day 1
+      calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'font-lock-function-name-face))
+
+;; Don't bother
+(setq confirm-kill-emacs nil)
 
 
 ;;
-;; Mail config
+;; Mail
 (setq send-mail-function 'sendmail-send-it
-        message-send-mail-function 'message-send-mail-with-sendmail
-        sendmail-program "msmtp")
+      message-send-mail-function 'message-send-mail-with-sendmail
+      sendmail-program "msmtp")
 
 
 ;;
-;; Writeroom
+;; Screencast
+(def-package! gif-screencast
+  :defer t
+  :config
+  (with-eval-after-load 'gif-screencast
+    (define-key gif-screencast-mode-map (kbd "<f12>") 'gif-screencast-toggle-pause)
+    (define-key gif-screencast-mode-map (kbd "<f11>") 'gif-screencast-stop))
+)
+
+;; make window larger
+(setq command-log-mode-window-size 60)
+
+
+;;
+;; Shell/Terminal
+(after! eshell
+  (set-eshell-alias!
+   "ff"  "+ivy/projectile-find-file"
+   "fd"  "counsel-projectile-find-dir"
+   "/p" "+ivy/project-search"
+   "/d" "+ivy/project-search-from-cwd"
+   "d"   "deer $1"
+   "l"   "ls -l"
+   "la"  "ls -la"
+   "gl"  "(call-interactively 'magit-log-current)"
+   "gs"  "magit-status"
+   "gc"  "magit-commit"
+   "gbD" "my/git-branch-delete-regexp $1"
+   "gbS" "my/git-branch-match $1"
+   "rg"  "rg --color=always $*"
+   "bat" "my/eshell-bat $1")
+  (setenv "PAGER" "cat"))
+;; fish-like auto-suggestions
+(def-package! esh-autosuggest
+  :after eshell
+  :config
+  (add-hook 'eshell-mode-hook #'esh-autosuggest-mode)
+  (when (and (executable-find "fish")
+             (require 'fish-completion nil t))
+    (global-fish-completion-mode)))
+;; improved terminal emulator
+(after! vterm
+  (setq vterm-max-scrollback 50000))
+
+
+;;
+;; Writing
 (def-package! writeroom-mode
   :after org
   :init
@@ -98,12 +133,10 @@
 
 
 ;;
-;; TRAMP
-;; make tramp assume ssh to avoid typing it when connecting to remote host
-(setq tramp-default-method "ssh")
-;; solution for getting around a server with warning message about a not fully
-;; functional terminal. This is due to the fact that tramp is set to "dumb"
+;; Remote editing
 (with-eval-after-load 'tramp-sh
+  ;; solution for getting around a server with warning message about a not fully
+  ;; functional terminal. This is due to the fact that tramp is set to "dumb"
   ;; Found a similar problem to mine here:
   ;; http://emacs.1067599.n8.nabble.com/problem-getting-files-under-SunOS-from-cygwin-td277890.html
   ;; there is also an example there about an interactive user input version if I ever need that :)
@@ -124,64 +157,38 @@
   (add-to-list 'tramp-actions-before-shell
                '(my-tramp-press-return-prompt-regexp my-tramp-press-return-action)))
 
-(with-eval-after-load 'tramp
-;; Docker-Tramp
-(require 'docker-tramp))
-
-;; Ediff
-(add-hook 'ediff-prepare-buffer-hook #'outline-show-all)
-
-
-;; Command-log-mode
-(setq command-log-mode-window-size 60)
-
-
-;; Flycheck
-;; disable using hooks
-(add-hook 'text-mode-hook (lambda ()
-                            (flycheck-mode -1)))
-(add-hook 'org-mode-hook (lambda ()
-                           (flycheck-mode -1)))
-
-
-;; Elisp
-(setq +pretty-code-enabled-modes '(emacs-lisp-mode org-mode))
 
 ;; Let the scratch buffer have elisp major mode by default
 ;; if set to t it has the same mode as previous buffer
 (setq doom-scratch-buffer-major-mode 'emacs-lisp-mode)
 
-;; Screencast
-(def-package! gif-screencast
-  :defer t
-  :config
-  (with-eval-after-load 'gif-screencast
-    (define-key gif-screencast-mode-map (kbd "<f12>") 'gif-screencast-toggle-pause)
-    (define-key gif-screencast-mode-map (kbd "<f11>") 'gif-screencast-stop))
-)
+;;
+;; Projects
+(setq projectile-enable-caching nil
+      projectile-project-search-path '("~/src/" "~/opensource"))
 
 
-;; Projectile
-(setq projectile-enable-caching nil)
-
-
-;; Pop-rule
+;;
+;; Windows (not the operating system)
 (after! org
   (set-popup-rule! "^\\*Org Agenda.*\\*$" :size 0.5 :side 'right :vslot 1  :select t :quit t   :ttl nil :modeline nil :autosave t)
   (set-popup-rule! "^CAPTURE.*\\.org$"    :size 0.4 :side 'bottom          :select t                                  :autosave t))
 
 
-;; Magit
-;; automatic spellchecking in commit messages
+;;
+;; Version control
+;; spell check commit messages
 (add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
-;; mitigate terminal is dumb
+;; mitigate dumb terminals
 (setenv "EDITOR" "emacsclient")
-;; submodules
+;; add submodules to magit-status
 (with-eval-after-load 'magit
 (magit-add-section-hook 'magit-status-sections-hook
                             'magit-insert-modules
                             'magit-insert-unpulled-from-upstream)
   (setq magit-module-sections-nested nil))
+;; improve diff of org-mode files
+(add-hook 'ediff-prepare-buffer-hook #'outline-show-all)
 
 
 ;;
@@ -203,37 +210,9 @@
               (start-process "switch-to-normal" nil "xkb-switch" "-s" normal-mode-keyboard-layout))))
 
 
+
 ;;
-;; eshell
-;; fish-like auto-suggestions
-(def-package! esh-autosuggest
-  :after eshell
-  :config
-  (add-hook 'eshell-mode-hook #'esh-autosuggest-mode)
-  (when (and (executable-find "fish")
-             (require 'fish-completion nil t))
-    (global-fish-completion-mode)))
-;; aliases
-(after! eshell
-  (set-eshell-alias!
-   "ff"  "+ivy/projectile-find-file"
-   "fd"  "counsel-projectile-find-dir"
-   "/p" "+ivy/project-search"
-   "/d" "+ivy/project-search-from-cwd"
-   "d"   "deer $1"
-   "l"   "ls -l"
-   "la"  "ls -la"
-   "gl"  "(call-interactively 'magit-log-current)"
-   "gs"  "magit-status"
-   "gc"  "magit-commit"
-   "gbD" "my/git-branch-delete-regexp $1"
-   "gbS" "my/git-branch-match $1"
-   "rg"  "rg --color=always $*"
-   "bat" "my/eshell-bat $1")
-  (setenv "PAGER" "cat"))
-
-
-;; ;; deer/ranger
+;; Dired/Ranger
 (after! ranger
   ;; Make the result of find-name-dired not open in a popup window but rather
   ;; reuse the current one
@@ -246,16 +225,16 @@
     (ranger-refresh))
   (advice-add 'find-name-dired :around #'my/find-name-dired))
 
-;; Snipe
-(evil-snipe-override-mode 1)
 
-
-;; Autoformat in C++ files using clang-format
+;;
+;; Auto-formatting
 ;; (add-hook 'c++-mode-hook #'+format|enable-on-save)
 (add-hook 'before-save-hook (lambda ()  (when (eq major-mode 'python-mode) (lsp-format-buffer))))
+(setq show-trailing-whitespace nil)
 
 
-;; Docsets
+;;
+;; Documentation
 (after! python
   (set-docsets! 'python-mode "Python 3" "NumPy" "SciPy" "Pandas"))
 (after! dockerfile
@@ -264,13 +243,13 @@
   (set-docsets! 'cmake-mode "CMake"))
 
 
-;; Matlab files (use octave mode)
+;;
+;; Mode file association
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
 (add-hook 'octave-mode-hook (lambda ()
                             (flycheck-mode -1)))
-
-;; Markdown
 (add-to-list 'auto-mode-alist '("\\.MD\\'" . markdown-mode))
+
 
 ;;
 ;; Load other config files
