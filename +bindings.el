71a56864d6ad9703dbb28632cb4a88ad8696e61a
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-07-15 16:43:59 +0200

    Minor change

--- +bindings.el
+++ +bindings.el
@@ -1,11 +1,11 @@
 ;;;  -*- lexical-binding: t; -*-
 
-;; Override other keys
 (map! :n "C-h" 'evil-window-left
       :n "C-j" 'evil-window-down
       :n "C-k" 'evil-window-up
       :n "C-l" 'evil-window-right
 
+      ;; evilify movement
       (:map evil-treemacs-state-map
         "C-h" 'evil-window-left
         "C-l" 'evil-window-right)
@@ -14,11 +14,13 @@
         "C-n" 'next-line-or-history-element
         "C-p" 'previous-line-or-history-element)
 
+      ;; use ediff for diffing in ranger
       (:map ranger-mode-map
         :nvmei ";=" #'+ora/ediff-files)
+
+      ;; create custom leader bindings
       (:leader
-        ;; Open
         (:prefix "o"
           :desc "Open debugger" :n "d" #'+dap-hydra/body
           :desc "Open mail" :n "m" #'=notmuch
-          :desc "Org-brain visualize" :n "v" #'org-brain-visualize)))
+          :desc "Open visualize brain" :n "v" #'org-brain-visualize)))
