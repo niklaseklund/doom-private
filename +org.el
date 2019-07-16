f7e793dd3bc5975d08d034c66c43880661fe724a
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-07-16 13:58:40 +0200

    Clean up Jira package

--- +org.el
+++ +org.el
@@ -72,58 +72,27 @@
                  ("\\paragraph{%s}" . "\\paragraph*{%s}"))))
 
 
+;;
 ;; Jira
 (def-package! org-jira
   :defer t
   :config
+  ;; Fix access
   (setq jiralib-url "https://jira.zenuity.com"
         org-jira-users `("Niklas Carlsson" . ,(shell-command-to-string "printf %s \"$(pass show work/zenuity/login | sed -n 2p | awk '{print $2}')\""))
-        jiralib-token `("Cookie". ,(my/init-jira-cookie))))
-
-;; Customization
-;; You can define one or more custom JQL queries to run and have your
-;; results inserted into, as such:
-(setq org-jira-custom-jqls
-      '(
-        (:jql " project = DUDE AND issuetype != Sub-task AND issuetype != Epic AND resolution = Unresolved AND  (Sprint = EMPTY OR Sprint NOT IN (openSprints(), futureSprints()))"
-              :limit 50
-              :filename "dude-backlog")
-        (:jql " project = DUDE AND issuetype != Sub-task AND sprint in openSprints() AND sprint NOT IN futureSprints()"
-              :limit 20
-              :filename "dude-current-sprint-user-stories")
-        (:jql " project = DUDE AND issuetype = Sub-task AND sprint in openSprints() AND sprint NOT IN futureSprints()"
-              :limit 50
-              :filename "dude-current-sprint-sub-tasks")
-        (:jql " project = DUDE AND issuetype = Epic"
-              :limit 20
-              :filename "dude-epics")
-        (:jql " project = DUDE AND assignee = currentuser() order by created DESC "
-              :limit 20
-              :filename "dude-niklas")
-        ))
-;; Please note this feature still requires some testing - things that
-;; may work in the existing proj-key named buffers (DUDE.org etc.) may
-;; behave unexpectedly in the custom named buffers.
-
-;; One thing you may notice is if you create an issue in this type of
-;; buffer, the auto-refresh of the issue will appear in the
-;; PROJ-KEY.org specific buffer (you will then need to refresh this
-;; JQL buffer by re-running the command C-c ij).
-
-;; The following variable, org-jira-worklog-sync-p, is set to true by
-;; default, but this causes an error on my machine when attempting to
-;; update issues. I believe I don't have the need for syncing the
-;; clocks.
-(setq org-jira-worklog-sync-p nil)
-
-;;Streamlined transition flow
-;; You can define your own streamlined issue progress flow as such:
-; If your Jira is set up to display a status in the issue differently
-; than what is shown in the button on Jira, your alist may look like
-; this (use the labels shown in the org-jira Status when setting it
-; up, or manually work out the workflows being used through standard
-; C-c iw options/usage):
- (defconst org-jira-progress-issue-flow
-   '(("To Do" . "In Progress")
-     ("In Progress" . "Review")
-     ("Review" . "Done")))
+        jiralib-token `("Cookie". ,(my/init-jira-cookie)))
+  ;; Customization jira query
+  (setq org-jira-custom-jqls
+        '(
+          (:jql " project = DUDE AND issuetype = Sub-task AND sprint in openSprints() AND sprint NOT IN futureSprints()"
+                :limit 50
+                :filename "dude-current-sprint-sub-tasks")
+          (:jql " project = DUDE AND assignee = currentuser() order by created DESC "
+                :limit 20
+                :filename "dude-niklas")
+          ))
+  ;; Customize the flow
+  (defconst org-jira-progress-issue-flow
+    '(("To Do" . "In Progress")
+      ("In Progress" . "Review")
+      ("Review" . "Done"))))
