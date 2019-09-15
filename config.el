435bc6a1ef0ec8553fce13cb5169eb5446a604e1
niklascarlsson <carlsson.niklas@gmail.com>
2019-09-15 23:09:49 +0200

    Remove an online provider that I don't use

--- config.el
+++ config.el
@@ -276,6 +276,8 @@
   (set-docsets! 'cmake-mode "CMake"))
 ;; add archwiki to online providers
 (add-to-list '+lookup-provider-url-alist '("ArchWiki" . "https://wiki.archlinux.org/index.php?search=%s"))
+;; remove providers that I don't use
+(setq +lookup-provider-url-alist (assoc-delete-all "Google images" +lookup-provider-url-alist))
 
 
 ;;
