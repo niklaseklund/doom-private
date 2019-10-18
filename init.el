87d31c22b1b80f214f78ef80fef54f80df57021c
niklascarlsson <carlsson.niklas@gmail.com>
2019-10-18 17:55:16 +0200

    Enable rest module

--- init.el
+++ init.el
@@ -147,7 +147,7 @@
         +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
-       ;;rest              ; Emacs as a REST client
+       rest              ; Emacs as a REST client
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
