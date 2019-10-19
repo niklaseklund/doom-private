db7be35f35087032640f375c215ecc1c7ae7d2ac
niklascarlsson <carlsson.niklas@gmail.com>
2019-10-19 16:53:27 +0200

    Update notmuch config

--- +mail.el
+++ +mail.el
@@ -6,32 +6,35 @@
 
 
 (use-package! counsel-notmuch
-  :when (featurep! :completion ivy)
   :commands counsel-notmuch
   :after notmuch)
+
 
 (after! notmuch
   ;; treat notmuch buffers as reall buffer (going from show->search will work)
   (add-hook 'doom-real-buffer-functions #'notmuch-interesting-buffer)
-  (add-hook 'notmuch-search-mode-hook #'hide-mode-line-mode)
+  (add-hook! '(notmuch-search-mode-hook
+               notmuch-show-mode-hook) #'hide-mode-line-mode)
 
   ;;
   ;; bindings
   ;; avoid killing notmuch search window
+  ;; TODO: create a better solution for not killing windows. I should have
+  ;; notmuch start window as default buffer?
   (map!
-   :map notmuch-search-mode-map
-   :nv "q" nil)
-  (map!
-   :map notmuch-tree-mode-map
-   :nv "d" nil))
+   (:map notmuch-search-mode-map
+    :nv "q" nil
+    :nv "C-s" #'counsel-notmuch)
+   (:map notmuch-tree-mode-map
+    :nv "d" nil))
 
-;; Other mail related settings
-(setq send-mail-function 'sendmail-send-it
-      notmuch-fcc-dirs nil              ; creates a copy of the mail, disable
-      message-send-mail-function 'message-send-mail-with-sendmail
-      sendmail-program "msmtp"
-      message-kill-buffer-on-exit t
-      notmuch-message-headers-visible nil)
+  ;; Other mail related settings
+  (setq send-mail-function 'sendmail-send-it
+        notmuch-fcc-dirs nil            ; creates a copy of the mail, disable
+        message-send-mail-function 'message-send-mail-with-sendmail
+        sendmail-program "msmtp"
+        message-kill-buffer-on-exit t
+        notmuch-message-headers-visible nil))
 
 ;; (defun +notmuch-dont-confirm-on-kill-process-a (orig-fn &rest args)
 ;;   "Don't prompt for confirmation when killing notmuch sentinel."
