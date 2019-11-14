84474330a9fc8b3297cdf3f1882d2e283b9b2d1d
Niklas Carlsson <carlsson.niklas@gmail.com>
2019-11-14 12:12:43 +0100

    Use ejira instead of org-jira

--- +org.el
+++ +org.el
@@ -59,4 +59,58 @@
 
 ;;
 ;; Jira
-(use-package ejira)
+(use-package! ejira
+  :defer t
+  :init
+  (setq jiralib2-url              "https://jira.zenuity.com"
+        jiralib2-auth             'cookie
+        jiralib2-user-login-name  "niklas.carlsson@zenuity.com"
+        jiralib2-token            nil
+
+        ejira-org-directory       (expand-file-name "ejira" doom-etc-dir)
+        ejira-projects            '("DUDE")
+
+        ejira-priorities-alist    '(("Highest" . ?A)
+                                    ("High"    . ?B)
+                                    ("Medium"  . ?C)
+                                    ("Low"     . ?D)
+                                    ("Lowest"  . ?E))
+        ejira-todo-states-alist   '(("To Do"       . 1)
+                                    ("In Progress" . 2)
+                                    ("Review"      . 3)
+                                    ("Done"        . 4)))
+
+  :config
+  ;; Tries to auto-set custom fields by looking into /editmeta
+  ;; of an issue and an epic.
+  (add-hook 'jiralib2-post-login-hook #'ejira-guess-epic-sprint-fields)
+
+  ;; They can also be set manually if autoconfigure is not used.
+  ;; (setq ejira-sprint-field       'customfield_10001
+  ;;       ejira-epic-field         'customfield_10002
+  ;;       ejira-epic-summary-field 'customfield_10004)
+
+  ;; Customize sprint
+  (setq ejira-sprint-field 'customfield_10004
+        ejira-scrum-project "DUDE")
+
+  ;; Agenda integration
+  (require 'ejira-agenda)
+  (add-to-list 'org-agenda-files ejira-org-directory)
+
+  ;; Add an agenda view with customized sections based on sprint content
+  (org-add-agenda-custom-command
+   '("j" "JIRA issues"
+     ((ejira-jql "project = DUDE and sprint = \"Zvision\" and status != Done and assignee = currentUser()"
+                 ((org-agenda-overriding-header "Assigned to me")))
+      (ejira-jql "project = DUDE and sprint = \"Zvision\" and status != Done and assignee is EMPTY"
+                 ((org-agenda-overriding-header "Unassigned")))
+      (ejira-jql "project = DUDE and sprint = \"Zvision\" and status != Done and assignee != currentUser()"
+                 ((org-agenda-overriding-header "Others"))))))
+
+  ;; Authentication
+  (require 'jiralib2)
+  (require 'auth-source)
+  ;; Figure out what is not loaded properly when I use the :after org config.
+  (jiralib2-session-login "niklas.carlsson@zenuity.com"
+                          (auth-source-pass-get 'secret "work/zenuity/login")))
