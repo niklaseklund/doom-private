5e45091c46deae86c6ac52319343413d682da7db
niklascarlsson <carlsson.niklas@gmail.com>
2019-11-15 11:43:06 +0100

    Add lsp for python remote
    Works on remote machines that uses pyenv. Otherwise need to modifiy the
    path to the pyls binary.

--- +lsp.el
+++ +lsp.el
@@ -9,6 +9,17 @@
         lsp-ui-doc-include-signature nil
         lsp-ui-doc-header nil
         lsp-file-watch-threshold 150000)
-  (add-to-list 'lsp-file-watch-ignored "build"))
+  (add-to-list 'lsp-file-watch-ignored "build")
+
+  ;; remote
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-tramp-connection "~/.pyenv/shims/pyls")
+                    :major-modes '(python-mode)
+                    :remote? t
+                    :server-id 'pyls-remote)))
+
+
+
 ;; lsp-formatting
 (add-hook 'before-save-hook (lambda ()  (when (eq major-mode 'python-mode) (lsp-format-buffer))))
+
