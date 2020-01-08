f4683a23eca6ec9d3f5be160c40d1faf76c9a251
Niklas Carlsson <carlsson.niklas@gmail.com>
2020-01-08 16:22:03 +0100

    Disable the pyenv python module flag
    I don't think I need this anymore if I use direnv. Will try without and
    see how that works.

--- init.el
+++ init.el
@@ -151,7 +151,7 @@
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python             ; beautiful is better than ugly
-        +pyenv
+        ;; +pyenv
         +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
