85670edddf36485a59ca2b71a3c0b5423a351c90
Niklas Carlsson <carlsson.niklas@gmail.com>
2020-01-13 16:23:30 +0100

    Improve documentation of function

--- autoload.el
+++ autoload.el
@@ -2,10 +2,12 @@
 
 ;;;###autoload
 (defun nc/transmission-start-daemon-a ()
-      "Start the transmission daemon."
-      (unless (member "transmission-da"
-                      (mapcar
-                       (lambda (pid) (alist-get 'comm (process-attributes pid)))
-                       (list-system-processes)))
-        (call-process "transmission-daemon")
-        (sleep-for 1)))
+  "Start the transmission daemon.
+The function is intended to be used to advice the transmission command. Ensuring
+that the daemon always runs when needed."
+  (unless (member "transmission-da"
+                  (mapcar
+                   (lambda (pid) (alist-get 'comm (process-attributes pid)))
+                   (list-system-processes)))
+    (call-process "transmission-daemon")
+    (sleep-for 1)))
