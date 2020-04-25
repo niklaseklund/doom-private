b0d5de8d80fd123e3c5886217f36746b9a309c45
Niklas Carlsson <niklas.carlsson@posteo.net>
2020-04-25 13:49:33 +0200

    Setup pretty code

--- init.el
+++ init.el
@@ -38,7 +38,8 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;pretty-code       ; ligatures or substitute text with pretty symbols
+       (pretty-code      ; ligatures or substitute text with pretty symbols
+        +fira)
        ;;tabs              ; an tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
