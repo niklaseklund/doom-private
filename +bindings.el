3a6696336c113098e5d6a4aa3bb77495309e9a1b
Niklas Carlsson <niklas.carlsson@posteo.net>
2020-04-25 13:49:13 +0200

    Slim down bindings

--- +bindings.el
+++ +bindings.el
@@ -1,25 +1,15 @@
 ;;;  -*- lexical-binding: t; -*-
 
-(map! :n "C-h" 'evil-window-left
-      :n "C-j" 'evil-window-down
-      :n "C-k" 'evil-window-up
-      :n "C-l" 'evil-window-right
-
-      ;; Use this binding to harmonize with ivy/swiper
-      :n "C-'" 'avy-goto-line
-
-      ;; window
-      (:leader
-        (:prefix "w"
-          :desc "Select window" :n "w" #'ace-window))
-
-      ;; create custom leader bindings
-      (:leader
-        :desc "Resume Avy" :n "\"" #'avy-resume
-        (:prefix "o"
-          :desc "Open brain" :n "b" #'org-brain-visualize
-          :desc "Open mail" :n "m" (lambda! (notmuch-search "tag:inbox"))
-          :desc "Open (pass-)store" :n "S" #'pass
-          :desc "Open (ivy-pass-)store" :n "s" #'ivy-pass)
-        (:prefix "s"
-          :desc "Search Youtube" :n "y" #'ivy-youtube)))
+(map!
+ ;; create custom leader bindings
+ (:leader
+   :desc "Resume Avy" :n "\"" #'avy-resume
+   (:prefix "o"
+     :desc "Open brain" :n "b" #'org-brain-visualize
+     :desc "Open mail" :n "m" (lambda! (notmuch-search "tag:inbox"))
+     :desc "Open (pass-)store" :n "S" #'pass
+     :desc "Open (ivy-pass-)store" :n "s" #'ivy-pass)
+   (:prefix "s"
+     :desc "Search Youtube" :n "y" #'ivy-youtube)
+   (:prefix "w"
+     :desc "Select window" :n "w" #'ace-window)))
