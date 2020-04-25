b5eed712a0538aa8c5a79f1084d26de1093d5792
Niklas Carlsson <niklas.carlsson@posteo.net>
2020-04-25 13:00:28 +0200

    Clean up bindings

--- +bindings.el
+++ +bindings.el
@@ -8,57 +8,18 @@
       ;; Use this binding to harmonize with ivy/swiper
       :n "C-'" 'avy-goto-line
 
-      ;; quit
-      "s-q" (λ! ()
-                (nc/delete-frame-and-stay))
-
-      ;; evilify movement
-      (:map evil-treemacs-state-map
-        "C-h" 'evil-window-left
-        "C-l" 'evil-window-right)
-
       ;; window
       (:leader
         (:prefix "w"
           :desc "Select window" :n "w" #'ace-window))
-
-      (:map minibuffer-local-map
-        "C-n" 'next-line-or-history-element
-        "C-p" 'previous-line-or-history-element)
-
-      ;; c++ bindings
-      (:localleader
-        :map c++-mode-map
-        (:prefix-map ("d" . "debug")
-          :desc "Set breakpoint" :n "b" #'gud-break
-          :desc "Remove breakpoint" :n "B" #'gud-remove
-          :desc "Jump here" :n "j" #'gud-jump
-          :desc "Continue" :n "c" #'gud-cont
-          :desc "Next" :n "n" #'gud-next
-          :desc "Step" :n "s" #'gud-step
-          :desc "Run" :n "r" #'gud-run
-          :desc "Up" :n "u" #'gud-up
-          :desc "Down" :n "d" #'gud-down
-          :desc "Kill" :n "K" #'nc/gud-kill-all-buffers
-          :desc "Start" :n "g" #'nc/gdb-mi-new-frame))
-
-      ;; common lisp bindings
-      (:localleader
-        :map lisp-mode-map
-        :desc "Sly connect" "\"" #'sly-connect)
 
       ;; create custom leader bindings
       (:leader
         :desc "Resume Avy" :n "\"" #'avy-resume
         (:prefix "o"
           :desc "Open brain" :n "b" #'org-brain-visualize
-          :desc "Open chat" :n "c" #'erc-switch-to-buffer
-          :desc "Open debugger" :n "d" #'+dap-hydra/body
           :desc "Open mail" :n "m" (lambda! (notmuch-search "tag:inbox"))
-          :desc "Open (pass-)store" :n "s" #'pass
-          :desc "Open (ivy-pass-)store" :n "S" #'ivy-pass
-          :desc "Open gerrit" :n "G" #'gci-list-changes)
+          :desc "Open (pass-)store" :n "S" #'pass
+          :desc "Open (ivy-pass-)store" :n "s" #'ivy-pass)
         (:prefix "s"
-          :desc "Search Youtube" :n "y" #'ivy-youtube)
-        (:prefix "h"
-          :desc "Keycast" :n "L" #'global-command-log-mode)))
+          :desc "Search Youtube" :n "y" #'ivy-youtube)))
