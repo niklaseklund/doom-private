d765562c0bf71cfa38aeafde0caed29f6bc5ff88
Niklas Carlsson <niklas.carlsson@posteo.net>
2020-04-26 21:14:15 +0200

    use-package for calendar

--- +system.el
+++ +system.el
@@ -127,14 +127,16 @@
 ;;
 ;; Calendar
 ;; Show week numbers in calendar
-(setq calendar-week-start-day 1
-      calendar-intermonth-text
-      '(propertize
-        (format "%2d"
-                (car
-                 (calendar-iso-from-absolute
-                  (calendar-absolute-from-gregorian (list month day year)))))
-        'font-lock-face 'font-lock-function-name-face))
+(use-package! calendar
+  :config
+  (setq calendar-week-start-day 1
+        calendar-intermonth-text
+        '(propertize
+          (format "%2d"
+                  (car
+                   (calendar-iso-from-absolute
+                    (calendar-absolute-from-gregorian (list month day year)))))
+          'font-lock-face 'font-lock-function-name-face)))
 
 
 ;;
